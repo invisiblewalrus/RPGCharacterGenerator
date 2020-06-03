612253f8ab78e443d846fd60e077f1317ff67b25
invisiblewalrus <mccoretso@gmail.com>
2020-06-02 23:38:10 -0500

    started styling page

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './cardstyle.module.css'
 
 export function CharacterCard(props){
 
@@ -6,8 +7,8 @@ export function CharacterCard(props){
         <li key={stat.name}>{stat.name + ": " + stat.value}</li>
     );
     return(
-    <div>
-        <h1>{props.character.name}</h1>
+    <div className={styles.main}>
+        <h1 className={styles.title}>{props.character.name}</h1>
         <h2>{props.character.species + " " + props.character.job}</h2>
         <ul>{statsList}</ul>
     </div>
